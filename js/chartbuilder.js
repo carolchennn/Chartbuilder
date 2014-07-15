a05a120c32a24eef818b5bc34c6f5779a336eb96
carolchennn <wuchencarol@gmail.com>
2014-07-15 17:45:50 -0400

    Update chartbuilder.js

--- js/chartbuilder.js
+++ js/chartbuilder.js
@@ -873,14 +873,19 @@ ChartBuilder.start = function(config) {
                                   html2canvas(document.getElementById("chartbigContainer"), {
                                               onrendered: function(tempcanvas) {
                                               var can = document.createElement('canvas');
-                                              can.width = $("#chartbigContainer").width() * 2;
-                                              can.height = $("#chartbigContainer").height() * 4;
+                                              // can.width = $("#chartbigContainer").width() * 2;
+                                              // can.height = $("#chartbigContainer").height() * 4;
+                                              can.width = $("#chartbigContainer").width();
+                                              can.height = $("#chartbigContainer").height() + 32;
                                               var ctx = can.getContext('2d');
                                               var framecontent = tempcanvas.getContext("2d");
                                               
                                               var canvas1 = document.getElementById("canvas");
-                                              canvas1.width = $("#chartContainer").width() * 2;
-                                              canvas1.height = $("#chartContainer").height() * 4;
+                                              //canvas1.width = $("#chartContainer").width() * 2;
+                                              //canvas1.height = $("#chartContainer").height() * 4;
+                                              
+                                              canvas1.width = $("#chartContainer").width();
+                                              canvas1.height = $("#chartContainer").height();
                                               var canvasContext1 = canvas1.getContext("2d");
                                             
                                               console.log("callback");
